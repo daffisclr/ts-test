0a1e1d0207893216bf28cd17e858f69f42cdbe48
daffisclr <daffisclr@gmail.com>
2024-06-10 19:59:10 +0700

    fix landingpage

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -18,7 +18,7 @@
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
 
     <!-- Favicon -->
-    <link href="{{ asset('img/favicon.png') }}" rel="icon" type="image/png">
+    <link href="{{ url('asset_pnj/Logo_pnj_favicon.jpg') }}" rel="icon">"
 </head>
 <body class="bg-gradient-primary min-vh-100 d-flex justify-content-center align-items-center">
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -658,30 +658,31 @@
                             <div class="address">
                                 <i class="bi bi-geo-alt"></i>
                                 <h4>Location:</h4>
-                                <p>A108 Adam Street, New York, NY 535022</p>
+                                <p>Jl. Prof. DR. G.A. Siwabessy, Kampus Universitas Indonesia Depok 16425 </p>
                             </div>
 
                             <div class="email">
                                 <i class="bi bi-envelope"></i>
                                 <h4>Email:</h4>
-                                <p>info@example.com</p>
+                                <p>tik@pnj.ac.id</p>
                             </div>
 
                             <div class="phone">
                                 <i class="bi bi-phone"></i>
                                 <h4>Call:</h4>
-                                <p>+1 5589 55488 55s</p>
+                                <p>021-7270036 ext 279</p>
                             </div>
 
                             <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621"
+                                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7930.359553089339!2d106.823671!3d-6.370776!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ec1cabb59bdf%3A0x28b4f84e4677f329!2sJakarta%20State%20Polytechnic!5e0!3m2!1sen!2sus!4v1718023240477!5m2!1sen!2sus"
                                 frameborder="0" style="border:0; width: 100%; height: 290px;"
-                                allowfullscreen></iframe>
+                                allowfullscreen>
+                            </iframe>
                         </div>
 
                     </div>
 
-                    {{-- <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+                    <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
                         <form action="forms/contact.php" method="post" role="form" class="php-email-form">
                             <div class="row">
                                 <div class="form-group col-md-6">
@@ -710,7 +711,7 @@
                             </div>
                             <div class="text-center"><button type="submit">Send Message</button></div>
                         </form>
-                    </div> --}}
+                    </div>
 
                 </div>
 
@@ -726,13 +727,13 @@
                 <div class="row">
 
                     <div class="col-lg-3 col-md-6 footer-contact">
-                        <h3>Arsha</h3>
+                        <h3>SITS-JTIK</h3>
                         <p>
-                            A108 Adam Street <br>
-                            New York, NY 535022<br>
-                            United States <br><br>
-                            <strong>Phone:</strong> +1 5589 55488 55<br>
-                            <strong>Email:</strong> info@example.com<br>
+                            Jl. Prof. DR. G.A. Siwabessy<br>
+                            Kampus Universitas Indonesia<br>
+                            Depok 16425<br><br>
+                            <strong>Phone:</strong> 021-7270036 ext 279<br>
+                            <strong>Email:</strong>  tik@pnj.ac.id<br>
                         </p>
                     </div>
 
@@ -762,11 +763,8 @@
                         <h4>Our Social Networks</h4>
                         <p>Cras fermentum odio eu feugiat lide par naso tierra videa magna derita valies</p>
                         <div class="social-links mt-3">
-                            <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                            <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                            <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                            <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                            <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                            <a href="https://www.instagram.com/jtik_pnj/?hl=en" class="instagram"><i class="bx bxl-instagram"></i></a>
+                            <a href="https://www.youtube.com/@TIKPNJ" class="youtube"><i class="bx bxl-youtube"></i></a>
                         </div>
                     </div>
 
@@ -776,14 +774,10 @@
 
         <div class="container footer-bottom clearfix">
             <div class="copyright">
-                &copy; Copyright <strong><span>Arsha</span></strong>. All Rights Reserved
+                &copy; Copyright <strong><span>Tracer Study Teknik Informatika dan Komputer Politeknik Negeri Jakarta</span></strong>. All Rights Reserved
             </div>
             <div class="credits">
-                <!-- All the links in the footer should remain intact. -->
-                <!-- You can delete the links only if you purchased the pro version. -->
-                <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                Designed by <a href="https://tik.pnj.ac.id/">TIK PNJ</a>
             </div>
         </div>
     </footer><!-- End Footer -->
