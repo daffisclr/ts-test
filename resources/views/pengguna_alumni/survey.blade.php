859f5c1e5dffb37077dbb855ccc68226d0b5b78c
daffisclr <daffisclr@gmail.com>
2024-06-12 15:57:31 +0700

    form pengguna alumni

--- resources/views/pengguna_alumni/survey.blade.php
+++ resources/views/pengguna_alumni/survey.blade.php
@@ -66,8 +66,8 @@
             <div class="container">
 
                 <ol>
-                    <li><a href="index.html">Home</a></li>
-                    <li>Inner Page</li>
+                    <li><a href="{{ url('/') }}">Home</a></li>
+                    <li>Survey Pengguna Alumni</li>
                 </ol>
                 <h2>Survey Pengguna Alumni Jurusan Teknik Informatika dan Komputer Politeknik Negeri Jakarta</h2>
 
@@ -104,6 +104,49 @@
                                     <input type="text" name="name" class="form-control" id="name" required>
                                 </div>
                             </div>
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <label for="name"><b>Nama Alumni JTIK yang dinilai</b></label>
+                                    <input type="text" name="name" class="form-control" id="name"
+                                        required>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="form-group col-md-6">
+                                    <label for="name"><b>Prodi</b></label>
+                                    <select id="p_alumni_program" name="alumni_program" class="form-control"
+                                        required>
+                                        <option selected="">Choose...</option>
+                                        <option value="1">Teknik Informatika</option>
+                                        <option value="2">Teknik Multimedia Jaringan</option>
+                                        <option value="3">Teknik Multimedia Digital</option>
+                                        <option value="3">Teknik Komputer Jaringan</option>
+                                    </select>
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <label for="name"><b>Jenjang</b></label>
+                                    <select id="p_alumni_program" name="alumni_program" class="form-control"
+                                        required>
+                                        <option selected="">Choose...</option>
+                                        <option value="1">D1</option>
+                                        <option value="2">D4</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="form-group col-md-12">
+                                    <label for="name"><b>Angkatan</b></label>
+                                    <select id="p_alumni_program" name="alumni_program" class="form-control"
+                                        required>
+                                        <option selected="">Choose...</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <label for="name"><b>Kesesuaian Keterampilan Lulusan</b></label>
+                                <p>Skor 1-5 (1 sangat rendah - 5 sangat tinggi)</p>
+                            </div>
+                            <br>
                             <div class="form-group">
                                 <label for="name"><b>Harapan/saran dalam rangka peningkatan kualitas lulusan
                                         JTIK</b></label>
