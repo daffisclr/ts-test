815f4b998f10dd0a21a03f1dc70ee7fc9b99b12a
daffisclr <daffisclr@gmail.com>
2024-07-16 13:49:44 +0700

    today

--- resources/views/assets/card.blade.php
+++ resources/views/assets/card.blade.php
@@ -1,11 +1,11 @@
 <div class="row">
-    <!-- Users -->
+    <!-- Jumlah Alumni JTIK (Total) -->
     <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-warning shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">{{ __('Alumni') }}
+                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">{{ __('Jumah Alumni JTIK') }}
                         </div>
                         <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $widget['users'] }}</div>
                     </div>
@@ -17,14 +17,15 @@
         </div>
     </div>
 
+    <!-- Jumlah Alumni Teknik Informatika -->
     <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-warning shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">{{ __('Total Responden') }}
+                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">{{ __('Jumlah Alumni Teknik Informatika') }}
                         </div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $widget['users'] }}</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $widget['alumni_ti'] }}</div>
                     </div>
                     <div class="col-auto">
                         <i class="fas fa-users fa-2x text-gray-300"></i>
@@ -34,14 +35,15 @@
         </div>
     </div>
 
+    <!-- Jumlah Alumni Teknik Multimedia dan Jaringan -->
     <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-warning shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">{{ __('Responden Valid') }}
+                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">{{ __('Jumlah Alumni Teknik Multimedia dan Jaringan') }}
                         </div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $widget['users'] }}</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $widget['alumni_tmj'] }}</div>
                     </div>
                     <div class="col-auto">
                         <i class="fas fa-users fa-2x text-gray-300"></i>
@@ -50,4 +52,59 @@
             </div>
         </div>
     </div>
+
+    <!-- Jumlah Alumni Teknik Multimedia Digital -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-warning shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">{{ __('Jumlah Alumni Teknik Multimedia Digital') }}
+                        </div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $widget['alumni_tmd'] }}</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-users fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Jumlah Alumni Teknik Komputer dan Jaringan -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-warning shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">{{ __('Jumlah Alumni Teknik Komputer dan Jaringan') }}
+                        </div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $widget['alumni_tkj'] }}</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-users fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Rata-rata penghasilan alumni -->
+    <div class="col-xl-3 col-md-6 mb-4">
+        <div class="card border-left-warning shadow h-100 py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">{{ __('Rata-rata Penghasilan Alumni') }}
+                        </div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $widget['pendapatan'] }}</div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-users fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
